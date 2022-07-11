0a5b8a556e5aea5d827aba8079b805d0fe95678f
Amina <42360103+aventon1@users.noreply.github.com>
2022-07-10 23:55:18 -0400

    Created HTTP request using Requests library
    Converted response data to dictionary

---
+++ inventory.py
@@ -0,0 +1,11 @@
+import requests
+import json
+
+# Submit a http request to website inventory and get a response
+response = requests.get("https://wss2.cex.uk.webuy.io/v3/boxes/SCPUINTI32100/neareststores?latitude=53.10040499999999&longitude=-2.4438209")
+
+# Convert Response object to string
+response_string = json.dumps(response.json())
+
+# Convert string to dictionary
+response_dict = json.loads(response_string)
